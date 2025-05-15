776dae1083c3fc8ba600ebb29f70e922d34432a8
EthanCS <ethancs1993@gmail.com>
2025-05-15 14:00:09 +0800

    update

--- src/WorldStateAsset.cpp
+++ src/WorldStateAsset.cpp
@@ -1,6 +1,113 @@
 #include "WorldStateAsset.h"
 
-void WorldStateEntryAsset::_bind_methods() {}
+void WorldStateEntryAsset::_bind_methods()
+{
+    BIND_ENUM_CONSTANT(Int);
+    BIND_ENUM_CONSTANT(Float);
+    BIND_ENUM_CONSTANT(Bool);
+
+    ClassDB::bind_method(D_METHOD("set_name", "name"), &WorldStateEntryAsset::set_name);
+    ClassDB::bind_method(D_METHOD("get_name"), &WorldStateEntryAsset::get_name);
+    ClassDB::bind_method(D_METHOD("set_supported_type", "type"), &WorldStateEntryAsset::set_supported_type);
+    ClassDB::bind_method(D_METHOD("get_supported_type"), &WorldStateEntryAsset::get_supported_type);
+
+    ClassDB::bind_method(D_METHOD("get_buffer_data"), &WorldStateEntryAsset::get_buffer_data);
+    ClassDB::bind_method(D_METHOD("set_buffer_data", "data"), &WorldStateEntryAsset::set_buffer_data);
+
+    ADD_PROPERTY(PropertyInfo(Variant::STRING, "name"), "set_name", "get_name");
+    ADD_PROPERTY(PropertyInfo(Variant::INT, "supported_type", PROPERTY_HINT_ENUM, "Int,Float,Bool"),
+                 "set_supported_type", "get_supported_type");
+    ADD_PROPERTY(
+        PropertyInfo(Variant::PACKED_BYTE_ARRAY, "buffer_data", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_STORAGE),
+        "set_buffer_data", "get_buffer_data");
+}
+
+void WorldStateEntryAsset::_get_property_list(List<PropertyInfo> *p_list) const
+{
+    Variant::Type type;
+    switch (supportedType)
+    {
+    case Int:
+        type = Variant::INT;
+        break;
+    case Float:
+        type = Variant::FLOAT;
+        break;
+    case Bool:
+        type = Variant::BOOL;
+        break;
+    default:
+        return;
+    }
+    p_list->push_back(PropertyInfo(type, "value"));
+}
+
+bool WorldStateEntryAsset::_get(const StringName &p_name, Variant &r_ret) const
+{
+    if (p_name == StringName("value"))
+    {
+        switch (supportedType)
+        {
+        case Int:
+            r_ret = *reinterpret_cast<const int32_t *>(buffer);
+            return true;
+        case Float:
+            r_ret = *reinterpret_cast<const float *>(buffer);
+            return true;
+        case Bool:
+            r_ret = *reinterpret_cast<const bool *>(buffer);
+            return true;
+        }
+    }
+    return false;
+}
+
+bool WorldStateEntryAsset::_set(const StringName &p_name, const Variant &p_value)
+{
+    if (p_name == StringName("value"))
+    {
+        switch (supportedType)
+        {
+        case Int:
+            *reinterpret_cast<int32_t *>(buffer) = p_value;
+            return true;
+        case Float:
+            *reinterpret_cast<float *>(buffer) = p_value;
+            return true;
+        case Bool:
+            *reinterpret_cast<bool *>(buffer) = p_value;
+            return true;
+        }
+    }
+    return false;
+}
+
+PackedByteArray WorldStateEntryAsset::get_buffer_data() const
+{
+    PackedByteArray arr;
+    arr.resize(kMaxBufferSize);
+    memcpy((void *)arr.ptrw(), buffer, kMaxBufferSize);
+    return arr;
+}
+
+void WorldStateEntryAsset::set_buffer_data(const PackedByteArray &p_data)
+{
+    memset(buffer, 0, kMaxBufferSize); // 清空缓冲区
+    if (p_data.size() > 0)
+    {
+        memcpy(buffer, p_data.ptr(), MIN(p_data.size(), kMaxBufferSize));
+    }
+}
+
+String WorldStateEntryAsset::get_name() const { return name; }
+void WorldStateEntryAsset::set_name(const String &p_name) { name = p_name; }
+
+WorldStateEntryAsset::SupportedType WorldStateEntryAsset::get_supported_type() const { return supportedType; }
+void WorldStateEntryAsset::set_supported_type(SupportedType p_type)
+{
+    supportedType = p_type;
+    memset(buffer, 0, kMaxBufferSize);
+}
 
 void WorldStateAsset::_bind_methods()
 {
--- src/WorldStateAsset.h
+++ src/WorldStateAsset.h
@@ -1,6 +1,7 @@
 #ifndef WORLD_STATE_ASSET_H
 #define WORLD_STATE_ASSET_H
 
+#include "godot_cpp/core/binder_common.hpp"
 #include <godot_cpp/classes/resource.hpp>
 
 using namespace godot;
@@ -16,14 +17,32 @@ public:
         Float,
         Bool
     };
+    static constexpr int kMaxBufferSize = 16;
 
 protected:
     static void _bind_methods();
 
+    bool _get(const StringName &p_name, Variant &r_ret) const;
+    bool _set(const StringName &p_name, const Variant &p_value);
+    void _get_property_list(List<PropertyInfo> *p_list) const;
+
 private:
     String name;
     SupportedType supportedType;
+    char buffer[kMaxBufferSize];
+
+    PackedByteArray get_buffer_data() const;
+    void set_buffer_data(const PackedByteArray &p_data);
+
+public:
+    String get_name() const;
+    void set_name(const String &p_name);
+
+    SupportedType get_supported_type() const;
+    void set_supported_type(SupportedType p_type);
 };
+
+VARIANT_ENUM_CAST(WorldStateEntryAsset::SupportedType);
 
 class WorldStateAsset : public Resource
 {
